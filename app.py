d47b2124d0fa53cbda3f34c63fc0fde18c86afea
Arun Nagath <arunnagath@gmail.com>
2022-07-23 13:21:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
-import urllib
 
 @app.route("/")
-
 def hello():
-    return urllib.urlopen('https://thumbs.dreamstime.com/z/hacker-picture-computer-44924818.jpg')
+    return "Hacked by ZEE APPsec"
 
