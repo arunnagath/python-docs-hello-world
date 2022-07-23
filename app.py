003e198f154ae85841ba88fdb6f02170eb5ec706
Arun Nagath <arunnagath@gmail.com>
2022-07-23 12:33:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from PIL import Image
+im=image.open("https://st2.depositphotos.com/2501025/5654/i/450/depositphotos_56545139-stock-photo-password.jpg")
+im.show()
 app = Flask(__name__)
 
 @app.route("/")
