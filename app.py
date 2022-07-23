bbd8bd27c0f4821884db880954a49b7476b749b5
Arun Nagath <arunnagath@gmail.com>
2022-07-23 13:07:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
+import urllib
+
+fun open():
+    return urllib.urlopen('https://thumbs.dreamstime.com/z/hacker-picture-computer-44924818.jpg')
 
 @app.route("/")
 def hello():
