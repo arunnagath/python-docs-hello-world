543fad004de2585482a31e8320f35ff0f6eb13b8
Arun Nagath <arunnagath@gmail.com>
2022-07-23 15:16:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hacked by ZEE APPsec"
-
+@app.route('/')
+def home():
+   return render_template('hacked.html')
+if __name__ == '__main__':
+   app.run()
