7ca7feda4a87eef2dcabd2313163df5a6f92b9eb
Arun Nagath <arunnagath@gmail.com>
2022-07-23 15:55:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-   return render_template('hacked.html')
-if __name__ == '__main__':
-   app.run()
+@app.route("/")
+def hello():
+    return "Hacked by alan"
