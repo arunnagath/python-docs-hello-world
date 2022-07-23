353a1f4fa3260648be3259cef6e0993558334b00
Arun Nagath <arunnagath@gmail.com>
2022-07-23 12:46:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
 from flask import Flask
-from PIL import Image
-import requests
-from io import BytesIO
-
-response = requests.get(https://st2.depositphotos.com/1000393/11789/i/950/depositphotos_117893952-stock-photo-hacker-with-mask.jpg)
-img = Image.open(BytesIO(response.content))
-app = Flask(__name__)
 
 @app.route("/")
 def hello():
